aadd6de7802c609e6c41c486c49a8948004608b8
trismagp <tris.magp@gmail.com>
2023-01-11 21:38:42 +0100

    lecture 277 create routes with templates

---
+++ twitter-clone/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
